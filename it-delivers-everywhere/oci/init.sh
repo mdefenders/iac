96942e2582cdab2681d12c23631968acd3fd582d
den <aditor@gmail.com>
2025-08-14 18:37:03 +0300

    minor fixes

--- it-delivers-everywhere/oci/init.sh
+++ it-delivers-everywhere/oci/init.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-#export OCI_USER=ocid1.user.oc1..aaaaaaaaalwx4pm7smbvxbpaqc4vybbx3it3cx3dd4anuepvdakiwtrplehq
-#export OCI_TENANCY=ocid1.tenancy.oc1..aaaaaaaab7gvagdtkiayq4tp6vzgm2npi2kqco4x4vaaxll6bapbhig354sa
-#export OCI_COMPARTMENT_ID=ocid1.tenancy.oc1..aaaaaaaab7gvagdtkiayq4tp6vzgm2npi2kqco4x4vaaxll6bapbhig354sa
-#export OCI_REGION=il-jerusalem-1
 STATE_BUCKET=adt-terraform-state-buckets
 
 # Check if bucket exists before creation
@@ -12,10 +8,3 @@ else
 oci os bucket create --name $STATE_BUCKET
 oci os bucket update --bucket-name $STATE_BUCKET --versioning Enabled
 fi
-#
-## Check if cluster exists before getting credentials
-#if gcloud container clusters describe $CLUSTER --zone $ZONE --project $PROJECT &>/dev/null; then
-#  gcloud container clusters get-credentials $CLUSTER --region=$ZONE --project=$PROJECT
-#else
-#  echo "Cluster $CLUSTER does not exist in project $PROJECT and zone $ZONE. Skipping get-credentials."
-#fi
